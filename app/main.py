443131c947fc89c53102ce1b00d500dbd90d7183
QA-Droid <redmindblow@gmail.com>
2024-12-12 19:46:09 +0200

    hw-06

--- app/main.py
+++ app/main.py
@@ -14,12 +14,12 @@ MONGO_DB = os.getenv("MONGO_DB", "message_db")
 
 
 class LogColors:
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
+    HEADER = "\033[35m"
+    OKBLUE = "\033[34m"
+    OKCYAN = "\033[36m"
+    OKGREEN = "\033[32m"
+    WARNING = "\033[33m"
+    FAIL = "\033[31m"
     ENDC = "\033[0m"
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
